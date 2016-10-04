4cc2400f6e6605925cd6ded3ea90163c310057e5
josh16 <josh.randall16@gmail.com>
2016-10-03 22:56:55 -0400

    Fixed Weapon
    Projectiles are shooting from BulletSpawner for the player.

---
+++ StudioLab_m1/Assets/Scripts/PlayerScripts/Shoot.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public float delayTime = 8f;
+
+    private float counter = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if(Input.GetKey(KeyCode.Mouse0) && counter > delayTime)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+
+            //audio will play
+            //audio.Play();
+            //setting counter to zero again
+            counter = 0;
+
+           /*
+            //Raycasting
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out hit,100f))
+            {
+                Instantiate()
+            }
+            */
+        }
+
+        counter += Time.deltaTime;
+    }
+}
---
+++ StudioLab_m1/Assets/Scripts/PlayerScripts/bulletmove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletmove : MonoBehaviour
+{
+    public float speed = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, speed);
+	}
+}
