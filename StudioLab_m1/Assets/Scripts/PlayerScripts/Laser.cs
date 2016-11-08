cfa06e8fd8656e446ce0f66bc1dde56a80a1d334
josh16 <josh.randall16@gmail.com>
2016-11-07 23:41:27 -0500

    CrossHair/Text UI
    -Added a crosshair for the player to see where to shoot
    -Added some minor text UI
    -Added a laser/line rendered Script!

---
+++ StudioLab_m1/Assets/Scripts/PlayerScripts/Laser.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer laser;
+
+	// Use this for initialization
+	void Start ()
+    {
+        laser = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position,transform.forward, out hit))
+        {
+            if(hit.collider)
+            {
+                laser.SetPosition(1, new Vector3(0, 0, hit.distance));
+            }else
+            {
+                laser.SetPosition(1, new Vector3(0, 0, 5000));
+            }
+        }
+
+
+	}
+
+
+}
